cd0372219c9921d1da0cc5e854a8ee627554c714
Stefano Paulo <stefanopaulo47@gmail.com>
2020-10-04 10:19:20 -0300

    modelo do curso de PHP

---
+++ exercicio.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/exercicio.css">
+    <title>Exercício</title>
+</head>
+<body class="exercicio">
+<header class="header">
+    <h1>Curso de PHP</h1>
+    <h2>Vizualição do Exercício</h2>
+</header>
+
+<nav class="navigation">
+    <a href=<?= "/{$_GET['dir']}/{$_GET['file']}.php" ?> class="green">Sem formatação</a>
+    <a href="index.php" class="red">Voltar</a>
+</nav>
+
+<main class="main">
+    <div class="content">
+        <?php
+            include("/{$_GET['dir']}/{$_GET['file']}.php")
+        ?>
+    </div>
+</main>
+
+<footer class="footer">
+    COD3R & ALUNOS &copy; <?= date('Y'); ?>
+</footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Curso PHP</title>
+</head>
+<body>
+    <header class="header">
+        <h1>Curso de PHP</h1>
+        <h2>Índice dos Exercícios</h2>
+    </header>
+    <main class="main">
+        <div class="content">
+            <nav class="modules">
+                <div class="module green">
+                    <h3>Módulo 01</h3>
+                    <ul>
+                        <li><a href="exercicio.php?dir=teste&file=teste">Exercício A</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </main>
+
+    <footer class="footer">
+        COD3R & ALUNOS &copy; <?= date('Y'); ?>
+    </footer>
+</body>
+</html>
