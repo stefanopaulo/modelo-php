5b3f8e50a719af30cbcbc7dab3c6f4a8233a8888
Stefano Paulo <stefanopaulo47@gmail.com>
2020-10-04 11:19:23 -0300

    comentários em PHP

---
+++ basico/comentarios.php
@@ -0,0 +1,19 @@
+<div class="title"> Comentários PHP </div>
+
+<?php
+    echo "Estou no PHP";
+    // echo "Cometário";
+    # echo "Comentário";
+// ?>
+
+<p>Depois do primeiro bloco!</p>
+
+<?php
+    /**
+     * Linha 1
+     * Linha 2
+     * Linha 3 
+     */
+?>
+
+<p>Depois do segundo bloco!</p>
